16474b2ed1df06cbe8e94b8b8a85d5b3694f960a
Robert Bryer <robert.bryer@bbc.co.uk>
2021-06-04 15:06:47 +0100

    Add scrappy webcodecs decoding prototype

--- waveform-data.js
+++ waveform-data.js
@@ -3,5 +3,6 @@
 var WaveformData = require("./lib/core");
 
 WaveformData.createFromAudio = require("./lib/builders/webaudio");
+WaveformData.createFromWebCodecs = require("./lib/builders/webcodecs");
 
 module.exports = WaveformData;
